3b7e871ba2872f5ad2a66a6c5d672dbed6181399
Apoll011 <tiagorobotik@gmail.com>
2024-06-21 09:36:36 -0100

    Added argparser to main

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from server import ApiServer
+from config import __version__
+import argparse
+import time
 
 server = ApiServer()
 
-server.serve()
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--list-routes", action="store_true", help="Will list all the routes installed")
+parser.add_argument("-s", "--start", action="store_true", help="Start The server")
+
+parser.add_argument("-v", "--version", action="version", version=f"Alex Base Api {__version__}")
+
+args = parser.parse_args()
+
+if args.list_routes or args.start:
+    if args.list_routes:
+        for route in server.api.defs:
+            print("\33[36mRoute:\33[32m", route, " \33[36mUp and running\33[0m")
+    if args.start:
+        if args.list_routes:
+            time.sleep(2)
+        server.serve()

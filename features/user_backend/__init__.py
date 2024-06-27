7bb57fe3e39d77753b689e4a0a1c707ca2438887
Apoll011 <tiagorobotik@gmail.com>
2024-06-27 15:16:10 -0100

    Added more info

--- features/user_backend/__init__.py
+++ features/user_backend/__init__.py
@@ -3,14 +3,14 @@ import os
 import uuid
 from enum import Enum
 
-class RelationshipType(Enum):
+class RelationshipType(int, Enum):
     """Enum for relationship types"""
     FRIEND = 1
     FAMILY = 2
     COLLEAGUE = 3
     NEIGHBOR = 4
 
-class TagType(Enum):
+class TagType(int, Enum):
     """Enum for tag types"""
     SOFTWARE_ENGINEER = 1
     DATA_SCIENTIST = 2
@@ -20,56 +20,96 @@ class TagType(Enum):
 class Person:
     """
     Represents a person with attributes like name, age, citizenship, etc.
-
-    Attributes:
-        id (str): Unique ID for the person
-        name (str): Name of the person
-        age (int): Age of the person
-        citizenship (str): Citizenship of the person
-        body_structure (dict): Body structure of the person (e.g. height, weight)
-        face_metadata (dict): Face metadata of the person (e.g. face ID)
-        tags (list): List of tags associated with the person
-        relationships (list): List of relationships associated with the person
     """
-    def __init__(self, name, age, citizenship, body_structure, face_metadata):
+    
+    id: str
+    """Unique ID for the person"""
+    name: str 
+    """Name of the person"""
+    age: int 
+    """Age of the person"""
+    citizenship: str 
+    """Citizenship of the person"""
+    body_structure: dict
+    """Body structure of the person (e.g. height, weight)"""
+    face_metadata: dict 
+    """Face metadata of the person (e.g. face ID)"""
+    tags:list 
+    """List of tags associated with the person"""
+    relationships: list
+    """List of relationships associated with the person"""
+    
+    def __init__(self, name = "", age = 0, citizenship = "", body_structure = dict(), face_metadata = dict()):
         self.id = str(uuid.uuid4())
         self.name = name
         self.age = age
         self.citizenship = citizenship
         self.body_structure = body_structure
         self.face_metadata = face_metadata
-        self.tags = []
-        self.relationships = []
 
+    def load(self, id, name, age, citizenship, body_structure, face_metadata, tags = [], relationships = []):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.citizenship = citizenship
+        self.body_structure = body_structure
+        self.face_metadata = face_metadata
+        self.tags = tags
+        self.relationships = relationships
+
+        return self
 class Relationship:
     """
     Represents a relationship between two people.
 
-    Attributes:
-        id (str): Unique ID for the relationship
-        person_id (str): ID of the person who initiated the relationship
-        related_person_id (str): ID of the person who is related to the initiator
-        relationship_type (RelationshipType): Type of relationship (e.g. friend, family, colleague)
     """
-    def __init__(self, person_id, related_person_id, relationship_type):
+    
+    id: str 
+    """Unique ID for the relationship"""
+    person_id: str
+    """ID of the person who initiated the relationship"""
+    related_person_id: str 
+    """ID of the person who is related to the initiator"""
+    relationship_type: RelationshipType 
+    """Type of relationship (e.g. friend, family, colleague)"""
+
+    def __init__(self, person_id = "", related_person_id = "", relationship_type = RelationshipType):
         self.id = str(uuid.uuid4())
         self.person_id = person_id
         self.related_person_id = related_person_id
         self.relationship_type = relationship_type
+    
+    def load(self, id, person_id, related_person_id, relationship_type):
+        self.id = id
+        self.person_id = person_id
+        self.related_person_id = related_person_id
+        self.relationship_type = relationship_type
+
+        return self
 
 class Tag:
     """
     Represents a tag associated with a person.
-
-    Attributes:
-        id (str): Unique ID for the tag
-        person_id (str): ID of the person who the tag is associated with
-        tag_type (TagType): Type of tag (e.g. software engineer, data scientist)
     """
-    def __init__(self, person_id, tag_type):
+
+    id: str 
+    """Unique ID for the tag"""
+    person_id: str
+    """ID of the person who the tag is associated with"""
+    tag_type: TagType
+    """Type of tag (e.g. software engineer, data scientist)"""
+    
+    def __init__(self, person_id = "", tag_type = TagType):
         self.id = str(uuid.uuid4())
         self.person_id = person_id
         self.tag_type = tag_type
+    
+    def load(self, id, person_id, tag_type):
+        self.id = id
+        self.person_id = person_id
+        self.tag_type = tag_type
+
+        return self
 
 class Database:
     """
@@ -92,9 +132,10 @@ class Database:
         if os.path.exists(self.filename):
             with open(self.filename, 'r') as f:
                 data = json.load(f)
-                self.people = {p['id']: Person(**p) for p in data['people']}
-                self.relationships = {r['id']: Relationship(**r) for r in data['relationships']}
-                self.tags = {t['id']: Tag(**t) for t in data['tags']}
+                print(data['people'])
+                self.people = {p['id']: Person().load(**p) for p in data['people']}
+                self.relationships = {r['id']: Relationship().load(**r) for r in data['relationships']}
+                self.tags = {t['id']: Tag().load(**t) for t in data['tags']}
 
     def save_data(self):
         data = {
@@ -180,6 +221,6 @@ relationship = api.create_relationship({"person_id": person1.id, "related_person
 tag = api.create_tag({"person_id": person1.id, "tag_type": TagType.SOFTWARE_ENGINEER})
 
 # retrieve data
-print(api.get_person(person1.id))  # Person(id=..., name=John Doe, age=30)
+print(api.get_person(person1.id).name)  # Person(id=..., name=John Doe, age=30)
 print(api.get_relationships())  # [Relationship(id=..., person_id=..., related_person_id=..., type=FRIEND)]
 print(api.get_tags())  # [Tag(id=..., person_id=..., tag_type=SOFTWARE_ENGINEER)]

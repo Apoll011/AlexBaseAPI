19131a49e2340451c8bba9da68eac4bd8505e4fa
Apoll011 <tiagorobotik@gmail.com>
2024-08-31 18:23:34 -0100

    imporved docs and added models

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import uvicorn
+from models import *
 from features.kit import *
-from config import __version__  
 from fastapi import FastAPI, Query
+from config import __version__ 
+from typing_extensions import Annotated
 
 app = FastAPI(title="Alex Server", version=__version__, description="Alex api server that handes complex and heavy tasks such an nlp user managements etc.", summary="Alex base server used for handling heavy functions", contact={"name": "Tiago Bernardo", "email": "tiagorobotik@gmail.com"}, license_info={"name": "Apache 2.0","url": "https://www.apache.org/licenses/LICENSE-2.0.html",})
 
@@ -10,7 +12,7 @@ userKit = UserKit()
 intentKit = IntentKit()
 dictionaryKit = DictionaryKit()
 
-@app.get("/alex/alive", name="Check If Alive", description="This checks if ale is running and send the basic values")
+@app.get("/alex/alive", name="Check If Alive", description="This checks if alex is running and send the basic values")
 async def alive():
     responce = {
         "on": True,
@@ -33,7 +35,7 @@ async def alive():
 async def intent_train():
     return {"responce": intentKit.train()}
 
-@app.get("/intent_recognition/get/reuse")
+@app.get("/intent_recognition/get/reuse", name="Load saved Engine", description="This will load teh saved intent recognition engine to memory")
 async def intent_reuse():
     try:
         intentKit.reuse()
@@ -41,48 +43,51 @@ async def intent_reuse():
     except Exception:
         return {"responce": False}
 
-@app.get("/intent_recognition/parse")
-def intent_reconize(text: str):
-    return intentKit.parse(text)
+@app.get("/intent_recognition/parse", name="Recognize intent from sentence", description="This will recognize the intent from a givin sentence and return the result parsed")
+async def intent_reconize(text: Annotated[str, Query(max_length=250, min_length=2)]):
+    try:
+        return intentKit.parse(text)
+    except AttributeError:
+        return {"error": "Engine not trained"}
 
-@app.get("/users/get/name")
-def user_search_name(name: str):
+@app.get("/users/get/name", name="Search user by name")
+async def user_search_name(name: Annotated[str, Query(max_length=65, min_length=2)]):
     return {"users": userKit.searchUser.by_name(name)}
 
-@app.get("/users/search/tags")
-def user_search(value: dict):
-    if "condition" in value.keys() and "exclude" in value.keys():
-        return {"users": userKit.searchUser.by_tags(value["query"], value["condition"], value["exclude"])}
-    elif "condition" in value.keys():
-        return {"users": userKit.searchUser.by_tags(value["query"], value["condition"])}
-    elif "exclude" in value.keys():
-        return {"users": userKit.searchUser.by_tags(value["query"], exclue=value["exclude"])}
-    elif  "query" not in value.keys():
-        return {"error": "Query required"}
-    else:
-        return {"users": userKit.searchUser.by_tags(value["query"])}
+@app.get("/users/search/tags", name="Search users by tags", description="Will search user using the tags each one has. Query is the tags name, exclude is a list of ids the exclud from the result, condition is (The sign to compare to the intensity: <, >, <=, >=, !=, =):(the Intensity of the tag) ex query=Friend, exclude=['0000000001(Master user id)'], condition = '>:50'. will return all the more that 50% friends excluding the master user.")
+async def user_search(search: TagsSearch):
+    try:
+        return {"users": userKit.searchUser.by_tags(search.query, search.condition, search.exclude)}
+    except Exception:
+        return {"error": "An error occurered"}
 
-@app.get("/user/get")
-def user_get(id: str):
+@app.get("/user/get", name="Get user object", description="gets an user object from a given id eg: 0815636592")
+async def user_get(id: Annotated[str, Query(max_length=10, min_length=10)]):
     return userKit.getUser.by_id(id)
 
-@app.get("/user/create")
-def user_create(user):
-    return userKit.createUser(user)
+@app.put("/user/create", name="Create user", description="Will create an user from an user object")
+async def user_create(user: User):
+    try:
+        userKit.createUser(user)
+        return {"responce":True}
+    except Exception:
+        return {"error": "Wont abble to create user"}
 
-@app.get("/dictionary/get")
-def dict_get(word: str):
+@app.get("/dictionary/get", name="Get word from dictionary", description="Will return the definition of a given word. Return null if no match is found.")
+async def dict_get(word: Annotated[str, Query(max_length=25, min_length=2)]):
     return dictionaryKit.get(word)
 
-@app.get("/dictionary/get/closest")
-def dict_get_cosest(word:str):
+@app.get("/dictionary/get/closest", name="Get the closes match in Dictionary", description="Will get the closes match of the word found in the dictionary. and return it and others matches too. Return null if no match is found.")
+async def dict_get_cosest(word: Annotated[str, Query(max_length=25, min_length=2)]):
     return dictionaryKit.get_closest(word)
 
-@app.get("/dictionary/load")
-def load_dic():
-    dictionaryKit.load()
-    return {"responce": True}
-
+@app.get("/dictionary/load", name = "Load the dictionary", description="Loads the dictionary file based on the given language.")
+async def load_dic(lang:Lang = Lang.EN):
+    try:
+        dictionaryKit.load(lang)
+        return {"responce": True}
+    except Exception:
+        return {"error": "Unable to load dictionary"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

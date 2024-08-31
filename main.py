0aa75101b30fa65b09e11bfe4f79e48e565e359b
Apoll011 <tiagorobotik@gmail.com>
2024-08-31 17:18:22 -0100

    changhed api methods

--- main.py
+++ main.py
@@ -1,30 +1,88 @@
-import time
-import argparse
-from api.server import API
-from config import __version__, api
-from routes import blueprint_list
-from features.kit import IntentKit
+import os
+import uvicorn
+from features.kit import *
+from config import __version__  
+from fastapi import FastAPI, Query
 
-server = API(api['HOST'], api['PORT'])
-server.register_blueprint_list(blueprint_list)
+app = FastAPI(title="Alex Server", version=__version__, description="Alex api server that handes complex and heavy tasks such an nlp user managements etc.", summary="Alex base server used for handling heavy functions", contact={"name": "Tiago Bernardo", "email": "tiagorobotik@gmail.com"}, license_info={"name": "Apache 2.0","url": "https://www.apache.org/licenses/LICENSE-2.0.html",})
 
-server.client_name = "Alex"
+userKit = UserKit()
+intentKit = IntentKit()
+dictionaryKit = DictionaryKit()
 
-parser = argparse.ArgumentParser()
+@app.get("/alex/alive", name="Check If Alive", description="This checks if ale is running and send the basic values")
+async def alive():
+    responce = {
+        "on": True,
+        "kit": {
+            "all_on": True and intentKit.loaded and  dictionaryKit.loaded,
+            "user": True,
+            "intent": intentKit.loaded,
+            "dictionary": dictionaryKit.loaded
+        },
+        "users": len(userKit.users), 
+        "lang": {
+            "trained": list(map(lambda e: e[6:-5], list(filter(lambda e: e.endswith(".yaml"),os.listdir("./features/intent_recognition/snips/data/"))))), 
+            "instaled": list(map(lambda e: e[9:-4], list(filter(lambda e: e.endswith(".json"),os.listdir("./features/intent_recognition/snips/dataset/"))))), 
+        },
+        "version": __version__
+        }
+    return responce
 
-parser.add_argument("-s", "--start", action="store_true", help="Start The server")
-parser.add_argument("-t", "--train", action="store_true", help="Train the intent engine and exit")
+@app.get("/intent_recognition/get/train", name="Train the Intent Recognition Engine")
+async def intent_train():
+    return {"responce": intentKit.train()}
 
-parser.add_argument("-v", "--version", action="version", version=f"Alex Base Api {__version__}")
+@app.get("/intent_recognition/get/reuse")
+async def intent_reuse():
+    try:
+        intentKit.reuse()
+        return {"responce": True}
+    except Exception:
+        return {"responce": False}
 
-args = parser.parse_args()
+@app.get("/intent_recognition/parse")
+def intent_reconize(text: str):
+    return intentKit.parse(text)
 
-if args.start or args.train:
-    if args.train:
-        time_stared = time.time()
-        print("\33[34mRe-trainig everyting. This process take around 1 to 2 minutes please wait...\33[0m")
-        IntentKit().train()
-        print("\33[93mTime Took:", time.time() - time_stared, "seconds")
-    
-    if args.start:
-        server.serve()
+@app.get("/users/get/name")
+def user_search_name(name: str):
+    return {"users": userKit.searchUser.by_name(name)}
+
+@app.get("/users/search/tags")
+def user_search(value: dict):
+    if "condition" in value.keys() and "exclude" in value.keys():
+        return {"users": userKit.searchUser.by_tags(value["query"], value["condition"], value["exclude"])}
+    elif "condition" in value.keys():
+        return {"users": userKit.searchUser.by_tags(value["query"], value["condition"])}
+    elif "exclude" in value.keys():
+        return {"users": userKit.searchUser.by_tags(value["query"], exclue=value["exclude"])}
+    elif  "query" not in value.keys():
+        return {"error": "Query required"}
+    else:
+        return {"users": userKit.searchUser.by_tags(value["query"])}
+
+@app.get("/user/get")
+def user_get(id: str):
+    return userKit.getUser.by_id(id)
+
+@app.get("/user/create")
+def user_create(user):
+    return userKit.createUser(user)
+
+@app.get("/dictionary/get")
+def dict_get(word: str):
+    return dictionaryKit.get(word)
+
+@app.get("/dictionary/get/closest")
+def dict_get_cosest(word:str):
+    return dictionaryKit.get_closest(word)
+
+@app.get("/dictionary/load")
+def load_dic():
+    dictionaryKit.load()
+    return {"responce": True}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

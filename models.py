1de84cfb48a6fbea16bd6f921148773e5dd6a1f2
Apoll011 <tiagorobotik@gmail.com>
2024-09-01 16:45:59 -0100

    Fixed Bug

--- models.py
+++ models.py
@@ -24,15 +24,10 @@ class Data(BaseModel):
     psycho_map: Dict[str, str] = {}
     citizenship: Citizenship
 
-class Tag(BaseModel):
-    label: str
-    value: str
-
 class User(BaseModel):
     name: str
     data: Data
-    tags: List[List[Tag]] = []
-    id: str
+    tags: List[List[str]] = []
 
 class Lang(str, Enum):
     EN = "en"

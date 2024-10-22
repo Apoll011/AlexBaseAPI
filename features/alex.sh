54b10404b6b21b5c3818e9982c36b8dfceb2d730
Apoll011 <tiagorobotik@gmail.com>
2024-10-22 19:30:59 -0100

    Fixed bug in linker. After first install the linker would crash with the existance of the previous linking file. added flag -f to link forcefully link alex.

--- features/alex.sh
+++ features/alex.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Define constants
-HOME_LOCATION = "/home/$USER"
-PID_FILE="$HOME_LOCATION/.alex"
-LOCATION="/bin/alex"
+export HOME_LOCATION="/home/$USER"
+export PID_FILE="$HOME_LOCATION/.alex"
+export LOCATION="/bin/alex"
 
 install() {
   echo "Installing Alex..."
   mkdir /tmp/alex
   echo "Downloading Alex..."
-  wget "http://$2:1178/version_control/main/get" -O /tmp/alex/alex.zip -t 5 -q
+  wget "http://$1:1178/version_control/main/get" -O /tmp/alex/alex.zip -t 5 -q
   echo "Downloaded Alex..."
   echo "Building Alex..."
   unzip -qq /tmp/alex/alex.zip -d /tmp/alex/
   echo "Linking..."
   cp /tmp/alex/main $HOME_LOCATION/alex
-  sudo ln -s $HOME_LOCATION/alex /bin/alex
+  sudo ln -s $HOME_LOCATION/alex /bin/alex -f
   rm -r /tmp/alex
   echo "Alex Installed..."
 }
@@ -28,7 +28,7 @@ uninstall(){
 
 case "$1" in
     install)
-        install
+        install $2
         ;;
     uninstall)
         uninstall
